b935c2bc10eae9795cc92d80e13715f8cee26418
Prashant Cholachagudda <prchol@microsoft.com>
2017-11-06 12:45:05 +0530

    Add project files.

---
+++ UnitTestApp1/TestInstrumentation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Android.App;
+using Android.Runtime;
+using Xamarin.Android.NUnitLite;
+using System.Reflection;
+
+namespace UnitTestApp1
+{
+    [Instrumentation(Name ="app.tests.TestInstrumentation")]
+    public class TestInstrumentation : TestSuiteInstrumentation
+    {
+        protected TestInstrumentation(IntPtr handle, JniHandleOwnership transfer) : base(handle, transfer)
+        {
+        }
+
+        protected override void AddTests()
+        {
+            AddTest(Assembly.GetExecutingAssembly());
+        }
+    }
+}
